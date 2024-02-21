2afd6c96de032642366754c1d5ccca1fb1481649
devmahin <mahinhowlader123@gmail.com>
2024-02-21 19:19:30 +0600

    let const

---
+++ script.js
@@ -0,0 +1,28 @@
+// const money = 50;
+// money = 50;   
+
+
+// let money2 = 50;
+// money2 = 100;   
+// console.log(money2)
+
+// const array = [343,343,4545,454];
+// array = [34, 454,454,4]
+// console.log(array)
+
+
+// let array2 = [343,343,4545,454];
+// array2 = [3434343,434]
+// console.log(array2)
+
+
+
+// const obj = {
+//     name : "mahin"
+// }
+
+// obj = {
+//     FirstName : "MAhin"
+
+// }
+// console.log(obj)
